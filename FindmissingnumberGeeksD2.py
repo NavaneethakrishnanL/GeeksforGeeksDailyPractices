d57a266bf29371a782aeaa2aac9edf7ef89ddf44
Navaneetha Krishnan <55486323+NavaneethakrishnanL@users.noreply.github.com>
2023-11-17 21:34:10 +0530

    Find the missing number

---
+++ FindmissingnumberGeeksD2.py
@@ -0,0 +1,42 @@
+'''
+Find the Missing Number
+Given an array of size N-1 such that it only contains distinct integers in the range of 1 to N. Find the missing element.
+
+Example 1:
+
+Input:
+N = 5
+A[] = {1,2,3,5}
+Output: 4
+Example 2:
+
+Input:
+N = 10
+A[] = {6,1,2,8,3,4,7,10,5}
+Output: 9
+
+Your Task :
+You don't need to read input or print anything. Complete the function MissingNumber() that takes array and N as input  parameters and returns the value of the missing number.
+
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+
+Constraints:
+1 ≤ N ≤ 106
+1 ≤ A[i] ≤ 106
+
+'''
+
+
+
+class Solution:
+    def missingNumber(self, array, n):
+        # Create a set for faster lookups
+        array_set = set(array)
+
+        # Check for missing elements
+        for i in range(1, n + 1):
+            if i not in array_set:
+                return i
